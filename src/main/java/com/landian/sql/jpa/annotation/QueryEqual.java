3516317f5a8231693e21a854283e23df1346f584
caojieliang <jieliangcao@163.com>
2017-09-04 10:21:59 +0800

    增加QueryEqual注解，支持字符串指定精确查询

---
+++ src/main/java/com/landian/sql/jpa/annotation/QueryEqual.java
@@ -0,0 +1,8 @@
+package com.landian.sql.jpa.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface QueryEqual {
+}
